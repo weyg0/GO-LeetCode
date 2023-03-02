5b66afb077f26e30966190f77525063ed5176f7d
weygo7 <1193063312@qq.com>
2023-03-02 15:07:16 +0800

    142

--- solutions/142. Linked List Cycle II.go
+++ solutions/142. Linked List Cycle II.go
@@ -8,15 +8,32 @@ package solutions
  * }
  */
 
+//func detectCycle(head *ListNode) *ListNode {
+//	nodeSet := make(map[*ListNode]bool)
+//	for head != nil {
+//		if nodeSet[head] {
+//			return head
+//		} else {
+//			nodeSet[head] = true
+//		}
+//		head = head.Next
+//	}
+//	return nil
+//}
+
 func detectCycle(head *ListNode) *ListNode {
-	nodeSet := make(map[*ListNode]bool)
-	for head != nil {
-		if nodeSet[head] {
-			return head
-		} else {
-			nodeSet[head] = true
+	fast, slow := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			fast = head
+			for fast != slow {
+				fast = fast.Next
+				slow = slow.Next
+			}
+			return fast
 		}
-		head = head.Next
 	}
 	return nil
 }
